4747b5273045d3e5b879a3e20123a650c4cee043
Eugene Sanduelnko <eugene.s@langate.com>
2018-05-20 09:52:51 -0400

    UI changes

--- AngelHack/AngelHack/Models/MainVM.cs
+++ AngelHack/AngelHack/Models/MainVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +10,13 @@ namespace AngelHack.Models
     public class MainVM
     {
         public List<SelectListItem> locationList { get; set; }
+
+        [Display(Name = "Location Type")]
         public string locationSelected { get; set; }
 
         public List<SelectListItem> StudioTypeList { get; set; }
+
+        [Display(Name = "Studio Type")]
         public string StudioTypeselected { get; set; }
 
         public string Title { set; get; }
